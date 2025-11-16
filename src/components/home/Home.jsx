c42b7097aea50fc70235eda27c4d5d589468105f
Shenol Shengov <shenolshengov41@gmail.com>
2025-11-16 06:58:09 +0200

    feat: add home base component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,7 @@
+import Container from "../ui/Container";
+
+export default function Home() {
+  return (
+    <Container className="flex-col justify-center items-center gap-52"></Container>
+  );
+}
