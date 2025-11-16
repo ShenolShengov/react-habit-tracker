c2524a84af4fc6800a7d9c2577d5810c9a445e9d
Shenol Shengov <shenolshengov41@gmail.com>
2025-11-16 06:46:39 +0200

    feat: set up mantine ui library

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@mantine/core";
 
 export default function App() {
   return (
     <div>
       <h1 className="text-5xl font-bold font-outfit">Habit tracker</h1>
+      <Button>Manite set up</Button>
     </div>
   );
 }
