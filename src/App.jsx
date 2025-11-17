9d54da34537d7da8a568acf15d5c77596a5fe304
Shenol Shengov <shenolshengov41@gmail.com>
2025-11-17 17:05:07 +0200

    feat: add routing for home, register, login, logout and place holder for dashboard with layouts and guards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { BrowserRouter, Route } from "react-router";
-import Home from "./components/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router";
+import GuestGuard from "../guards/GuestGuard";
+import GuestLayout from "./GuestLayout";
+import Home from "../components/home/Home";
+import AuthLayout from "./AuthLayout";
+import Login from "../components/login/Login";
+import Register from "../components/register/Register";
+import AuthGuard from "../guards/AuthGuard";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Route index element={<Home />} />
+      <Routes>
+        <Route element={<GuestGuard />}>
+          <Route element={<GuestLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Route>
+        <Route element={<AuthGuard />}>
+          <Route element={<Dashbard />}>
+            <Route path="/dashboard" element={<>Dasboard placeholder</>} />
+          </Route>
+          <Route element={<AuthLayout />}>
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
