e03e80793c03abe6e6976281b68a31e2251f1b82
Shenol Shengov <shenolshengov41@gmail.com>
2025-11-17 16:47:03 +0200

    feat: add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,35 @@
+import {
+  IconBrandInstagram,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+import { ActionIcon, Group } from "@mantine/core";
+import logo from "../assets/logo.png";
+import Container from "./ui/Container";
+
+export function Footer() {
+  return (
+    <div className="mt-32 border-t border-solid border-gray-300 ">
+      <Container className="justify-between items-center py-8 @max-xs:flex-col">
+        <img src={logo} alt="Habit tracker" className="h-10" />
+
+        <Group
+          gap={0}
+          className="@max-xs:mt-4"
+          justify="flex-end"
+          wrap="nowrap"
+        >
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </div>
+  );
+}
