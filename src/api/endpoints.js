d7d26d2868bc8f906c1e3ed53b4b5f6b08a4c732
Shenol Shengov <shenolshengov41@gmail.com>
2025-11-17 16:38:43 +0200

    Add endpoint module with basic endponinst of the api

---
+++ src/api/endpoints.js
@@ -0,0 +1,16 @@
+const enpoints = {
+  auth: {
+    register: "/auth/register",
+    login: "/auth/login",
+    logout: "/auth/logout",
+    refresh: "/auth/refresh",
+  },
+  user: {
+    me: "/me",
+  },
+  habits: {
+    base: "/habits",
+    byId: (id) => `/habits/${id}`,
+  },
+};
+export default enpoints;
