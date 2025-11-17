354861c7c506cd74453000b40c88d452f7a85f69
Shenol Shengov <shenolshengov41@gmail.com>
2025-11-17 16:44:00 +0200

    feat: set up react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { BrowserRouter, Route } from "react-router";
 import Home from "./components/home/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Route index element={<Home />} />
+    </BrowserRouter>
+  );
 }
